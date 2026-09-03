546bf78ac68560c8ca6fc17aa9843af8bcbe652f
Nathan Adams <npadams21@gmail.com>
2026-09-03 15:16:27 +0100

    add cors for locahost angular

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -13,6 +13,15 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,6 +32,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("AllowAngular");
 app.UseAuthorization();
 
 app.MapControllers();
